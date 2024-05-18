2bceb70930486e0838d34540128f22a1c5f9985e
FaraneysX <faraneys.up@gmail.com>
2024-05-19 00:52:06 +0300

    Добавлен инструмент для считывания конфига

--- ITCompany/src/main/java/ru/denisov/itcompany/utils/PropertyUtil.java
+++ ITCompany/src/main/java/ru/denisov/itcompany/utils/PropertyUtil.java
@@ -2,8 +2,11 @@ package ru.denisov.itcompany.utils;
 
 import java.io.IOException;
 import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class PropertyUtil {
+    private static final Logger LOGGER = Logger.getLogger(PropertyUtil.class.getName());
     private static final Properties PROPERTIES = new Properties();
 
     static {
@@ -14,10 +17,12 @@ public class PropertyUtil {
         return PROPERTIES.getProperty(key);
     }
 
-    private static void loadProperties() {
+    private static void loadProperties() throws RuntimeException {
         try (var iStream = PropertyUtil.class.getClassLoader().getResourceAsStream("application.properties")) {
             PROPERTIES.load(iStream);
         } catch (IOException e) {
+            LOGGER.log(Level.SEVERE, "Ошибка загрузки файла свойств: ", e);
+
             throw new RuntimeException(e);
         }
     }
