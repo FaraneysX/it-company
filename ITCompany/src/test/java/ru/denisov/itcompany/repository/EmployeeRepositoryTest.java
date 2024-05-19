ce6ae87b0ff5f37b335138e9eafe1c3d6d2de06d
FaraneysX <faraneys.up@gmail.com>
2024-05-19 16:44:54 +0300

    Добавлены тесты

--- ITCompany/src/test/java/ru/denisov/itcompany/repository/EmployeeRepositoryTest.java
+++ ITCompany/src/test/java/ru/denisov/itcompany/repository/EmployeeRepositoryTest.java
@@ -13,6 +13,7 @@ import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -31,7 +32,7 @@ class EmployeeRepositoryTest {
     private Connection connection;
     private PreparedStatement statement;
     private ResultSet resultSet;
-    private EmployeeRepository employeeRepository;
+    private EmployeeRepository repository;
 
     @BeforeAll
     static void init() {
@@ -80,17 +81,21 @@ class EmployeeRepositoryTest {
         connection = mock(Connection.class);
         statement = mock(PreparedStatement.class);
         resultSet = mock(ResultSet.class);
-        employeeRepository = new EmployeeRepository(connectionGetter);
+        repository = new EmployeeRepository(connectionGetter);
     }
 
     @Test
     void insert() throws SQLException, RepositoryException, InterruptedException {
         when(connectionGetter.get()).thenReturn(connection);
-        when(connection.prepareStatement(anyString())).thenReturn(statement);
+        when(connection.prepareStatement(anyString(), eq(Statement.RETURN_GENERATED_KEYS))).thenReturn(statement);
 
-        employeeRepository.insert(employee);
+        ResultSet generatedKeys = mock(ResultSet.class);
 
-        verify(connection).prepareStatement(anyString());
+        when(statement.getGeneratedKeys()).thenReturn(generatedKeys);
+        when(generatedKeys.next()).thenReturn(true);
+        when(generatedKeys.getLong("id")).thenReturn(1L);
+
+        repository.insert(employee);
 
         verify(statement).setLong(eq(1), eq(1L));
         verify(statement).setLong(eq(2), eq(1L));
@@ -98,13 +103,14 @@ class EmployeeRepositoryTest {
         verify(statement).setString(eq(4), eq("Иванов"));
 
         verify(statement).executeUpdate();
+
+        assertEquals(1L, employee.getId());
     }
 
     @Test
     void findAll() throws SQLException, InterruptedException {
         when(connectionGetter.get()).thenReturn(connection);
         when(connection.prepareStatement(anyString())).thenReturn(statement);
-
         when(statement.executeQuery()).thenReturn(resultSet);
 
         when(resultSet.next())
@@ -148,24 +154,85 @@ class EmployeeRepositoryTest {
                 .thenReturn("USER")
                 .thenReturn("ADMIN");
 
-        List<Employee> foundEmployees = employeeRepository.findAll();
+        List<Employee> foundEmployees = repository.findAll();
 
         verify(connection).prepareStatement(anyString());
-
         verify(statement).executeQuery();
 
         assertEquals(employees, foundEmployees);
     }
 
     @Test
-    void findById() {
+    void findById() throws InterruptedException, SQLException {
+        Long employeeId = 1L;
+
+        when(connectionGetter.get()).thenReturn(connection);
+        when(connection.prepareStatement(anyString())).thenReturn(statement);
+        when(statement.executeQuery()).thenReturn(resultSet);
+
+        when(resultSet.next()).thenReturn(true);
+        when(resultSet.getLong("id")).thenReturn(employee.getId());
+        when(resultSet.getLong("project_id")).thenReturn(employee.getProjectId());
+        when(resultSet.getLong("position_id")).thenReturn(employee.getPositionId());
+        when(resultSet.getString("name")).thenReturn(employee.getName());
+        when(resultSet.getString("surname")).thenReturn(employee.getSurname());
+        when(resultSet.getDate("birth_date")).thenReturn(Date.valueOf(employee.getBirthDate()));
+        when(resultSet.getString("email")).thenReturn(employee.getEmail());
+        when(resultSet.getString("password")).thenReturn(employee.getPassword());
+        when(resultSet.getString("role")).thenReturn(employee.getRole().toString());
+
+        Employee foundEmployee = repository.findById(employeeId);
+
+        verify(connection).prepareStatement(anyString());
+        verify(statement).setLong(eq(1), eq(1L));
+
+        verify(statement).executeQuery();
+
+        assertEquals(employee, foundEmployee);
     }
 
     @Test
-    void update() {
+    void update() throws InterruptedException, SQLException {
+        Employee updatedEmployee = new Employee(
+                1L,
+                2L,
+                2L,
+                "Иван",
+                "Иванов",
+                LocalDate.of(1990, 1, 1),
+                "ivanov@example.com",
+                "password",
+                Role.USER
+        );
+
+        when(connectionGetter.get()).thenReturn(connection);
+        when(connection.prepareStatement(anyString())).thenReturn(statement);
+
+        repository.update(updatedEmployee);
+
+        verify(connection).prepareStatement(anyString());
+        verify(statement).setLong(eq(6), eq(updatedEmployee.getId()));
+        verify(statement).setLong(eq(1), eq(updatedEmployee.getProjectId()));
+        verify(statement).setLong(eq(2), eq(updatedEmployee.getPositionId()));
+        verify(statement).setString(eq(3), eq(updatedEmployee.getEmail()));
+        verify(statement).setString(eq(4), eq(updatedEmployee.getPassword()));
+        verify(statement).setString(eq(5), eq(updatedEmployee.getRole().toString()));
+
+        verify(statement).executeUpdate();
     }
 
     @Test
-    void delete() {
+    void delete() throws SQLException, InterruptedException {
+        Long employeeId = 1L;
+
+        when(connectionGetter.get()).thenReturn(connection);
+        when(connection.prepareStatement(anyString())).thenReturn(statement);
+
+        repository.delete(employeeId);
+
+        verify(connection).prepareStatement(anyString());
+        verify(statement).setLong(eq(1), eq(employeeId));
+
+        verify(statement).executeUpdate();
     }
 }
